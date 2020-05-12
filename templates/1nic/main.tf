d22e70107598dcd09c87f5e3f2d05be8dd794b87
John Gruber <jgruber@f5.com>
2020-05-12 10:50:15 -0500

    renamed floating ip in 1NIC test template

--- templates/1nic/main.tf
+++ templates/1nic/main.tf
@@ -103,7 +103,7 @@ resource "ibm_is_instance" "f5_ve_instance" {
 }
 
 # create floating IPs
-resource "ibm_is_floating_ip" "f5_floating_ip" {
+resource "ibm_is_floating_ip" "f5_management_floating_ip" {
   name   = "f0-${random_uuid.namer.result}"
   target = ibm_is_instance.f5_ve_instance.primary_network_interface.0.id
 }
