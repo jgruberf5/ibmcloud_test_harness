3f019f985f12bb29767f83837145b19fb0c70f7b
John Gruber <jgruber@f5.com>
2020-05-29 10:38:57 -0500

    updated to new ibm terraform 1.7 syntax for icmp

---
+++ clean_up_timed_out_running.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+# coding=utf-8
+# pylint: disable=broad-except,unused-argument,line-too-long, unused-variable
+# Copyright (c) 2016-2018, F5 Networks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import sys
+import json
+import logging
+import datetime
+import time
+import requests
+import shutil
+import python_terraform as pt
+
+LOG = logging.getLogger('ibmcloud_test_process_running')
+LOG.setLevel(logging.DEBUG)
+FORMATTER = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOGSTREAM = logging.StreamHandler(sys.stdout)
+LOGSTREAM.setFormatter(FORMATTER)
+LOG.addHandler(LOGSTREAM)
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+RUNNING_DIR = "%s/running_tests" % SCRIPT_DIR
+
+CONFIG_FILE = "%s/builder-config.json" % SCRIPT_DIR
+CONFIG = {}
+
+
+def clean():
+    if os.path.exists(RUNNING_DIR):
+        test_dirs = os.listdir(RUNNING_DIR)
+        left_over_tests = []
+        for test_id in test_dirs:
+            left_over_tests.append(test_id)
+        report_url = "%s/report" % CONFIG['report_service_base_url']
+        resp = requests.get(report_url)
+        reports = resp.json()
+        output = {}
+        for test_id in reports:
+            test_dir = os.path.join(RUNNING_DIR, test_id)
+            if os.path.exists(test_dir):
+                LOG.debug("Destorying %s", test_dir)
+                tf = pt.Terraform(working_dir=test_dir, var_file='test_vars.tfvars')
+                tf.init()
+                tf.destroy()
+                shutil.rmtree(test_dir)
+        print(json.dumps(output, sort_keys=True,
+              indent=4, separators=(',', ': ')))
+
+
+def initialize():
+    global CONFIG
+    config_json = ''
+    with open(CONFIG_FILE, 'r') as cf:
+        config_json = cf.read()
+    CONFIG = json.loads(config_json)
+
+
+if __name__ == "__main__":
+    START_TIME = time.time()
+    LOG.debug('process start time: %s', datetime.datetime.fromtimestamp(
+        START_TIME).strftime("%A, %B %d, %Y %I:%M:%S"))
+    initialize()
+    ERROR_MESSAGE = ''
+    ERROR = False
+
+    clean()
+
+    STOP_TIME = time.time()
+    DURATION = STOP_TIME - START_TIME
+    LOG.debug(
+        'process end time: %s - ran %s (seconds)',
+        datetime.datetime.fromtimestamp(
+            STOP_TIME).strftime("%A, %B %d, %Y %I:%M:%S"),
+        DURATION
+    )
+
---
+++ templates/1nic_ibm_05292020/main.tf
@@ -0,0 +1,140 @@
+data "ibm_is_image" "tmos_image" {
+    name = var.tmos_image_name
+}
+
+data "ibm_is_subnet" "f5_subnet" {
+  identifier = var.subnet_id
+}
+
+data "ibm_is_ssh_key" "f5_ssh_pub_key" {
+  name = var.ssh_key_name
+}
+
+data "ibm_is_instance_profile" "instance_profile" {
+  name = var.instance_profile
+}
+
+data "template_file" "user_data" {
+  template = "${file("${path.module}/user_data.yaml")}"
+  vars = {
+    tmos_admin_password = var.tmos_admin_password
+    tmos_license_basekey = var.tmos_license_basekey
+    phone_home_url = var.phone_home_url
+  }
+}
+
+resource "random_uuid" "namer" { }
+
+// allow all traffic to data plane interfaces
+// TMM is the firewall
+resource "ibm_is_security_group" "f5_tmm_sg" {
+  name = "sd-${random_uuid.namer.result}"
+  vpc  = data.ibm_is_subnet.f5_subnet.vpc
+}
+
+// all TCP
+resource "ibm_is_security_group_rule" "f5_tmm_in_tcp" {
+  depends_on = [ibm_is_security_group.f5_tmm_sg]
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "inbound"
+  tcp {
+    port_min = 1
+    port_max = 65535
+  }
+}
+
+resource "ibm_is_security_group_rule" "f5_tmm_out_tcp" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_in_tcp]
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "outbound"
+  tcp {
+    port_min = 1
+    port_max = 65535
+  }
+}
+
+// all UDP
+resource "ibm_is_security_group_rule" "f5_tmm_in_udp" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_out_tcp]
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "inbound"
+  udp {
+    port_min = 1
+    port_max = 65535
+  }
+}
+
+resource "ibm_is_security_group_rule" "f5_tmm_out_udp" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_in_udp]  
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "outbound"
+  udp {
+    port_min = 1
+    port_max = 65535
+  }
+}
+
+// all ICMP
+resource "ibm_is_security_group_rule" "f5_tmm_in_icmp" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_out_udp] 
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "inbound"
+  icmp { }
+}
+
+resource "ibm_is_security_group_rule" "f5_tmm_out_icmp" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_in_icmp]
+  group     = ibm_is_security_group.f5_tmm_sg.id
+  direction = "outbound"
+  icmp { }
+}
+
+resource "ibm_is_instance" "f5_ve_instance" {
+  depends_on = [ibm_is_security_group_rule.f5_tmm_out_icmp]
+  name    = var.instance_name
+  image   = data.ibm_is_image.tmos_image.id
+  profile = data.ibm_is_instance_profile.instance_profile.id
+  primary_network_interface {
+    name            = "tmm-1nic"
+    subnet          = data.ibm_is_subnet.f5_subnet.id
+    security_groups = [ibm_is_security_group.f5_tmm_sg.id]
+  }
+  vpc  = data.ibm_is_subnet.f5_subnet.vpc
+  zone = data.ibm_is_subnet.f5_subnet.zone
+  keys = [data.ibm_is_ssh_key.f5_ssh_pub_key.id]
+  user_data = data.template_file.user_data.rendered
+}
+
+# create floating IPs
+resource "ibm_is_floating_ip" "f5_management_floating_ip" {
+  name   = "f0-${random_uuid.namer.result}"
+  target = ibm_is_instance.f5_ve_instance.primary_network_interface.0.id
+}
+
+output "resource_name" {
+  value = ibm_is_instance.f5_ve_instance.name
+}
+
+output "resource_status" {
+  value = ibm_is_instance.f5_ve_instance.status
+}
+
+output "VPC" {
+  value = ibm_is_instance.f5_ve_instance.vpc
+}
+
+output "f5_shell_access" {
+  value = "ssh://root@${ibm_is_floating_ip.f5_management_floating_ip.address}"
+}
+
+output "f5_admin_portal" {
+  value = "https://${ibm_is_floating_ip.f5_management_floating_ip.address}:8443"
+}
+
+output "f5_as_url" {
+  value = "https://${ibm_is_floating_ip.f5_management_floating_ip.address}:8443/mgmt/shared/appsvcs/declare"
+}
+
+output "test_type" {
+  value = var.test_type
+}
